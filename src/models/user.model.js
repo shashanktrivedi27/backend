6d6a6ef5ac479335196031d4d2c5c744f3df8b6d
Shashank Trivedi <shashanktrivedi0027@gmail.com>
2026-02-25 12:25:13 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String,//cloudinary url
+        required:true
+    },
+    coverImage:{
+        type:String,
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"video"
+        }
+    ],
+    password:{
+        type:String,
+        required:true
+    },
+    refreshTokens:{
+        type:String
+    }
+},{timestamps:true})
+
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password"))return next();
+    this.password=bcrypt.hash(this.password,8)
+    next()
+})
+
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccesTokens=function(){
+     return jwt.sign(
+        {
+        _id:this._id,
+        email:this.email,
+        useename:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {   
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+userSchema.methods.generateRefreshTokens=function(){
+    return jwt.sign(
+        {
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {   
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
+
+export const User=mongoose.model("User",userSchema)
